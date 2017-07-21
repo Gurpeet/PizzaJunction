e8860f7d2297d6b3b54b933ba340450fb19b8fb7
Gurpreet Singh <gopisivia@gmail.com>
2017-07-21 14:43:57 +0530

    Pickup case won't have checkbox "Same as Delivery Address"

--- client/src/app/menu/checkout/checkout.component.ts
+++ client/src/app/menu/checkout/checkout.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { StorageService } from './../../shared/services/storage.service';
 import { CartItem } from './../../shared/models/menuitem';
-import { Globals } from './../../shared/components/globals/global';
+import { Globals, orderType } from './../../shared/components/globals/global';
 import { Address } from './../../shared/models/address.model';
 
 @Component({
@@ -9,21 +9,24 @@ import { Address } from './../../shared/models/address.model';
 })
 export class CheckoutComponent implements OnInit {
     private cartDetails: CartItem;
+    private deliveryAddress: Address;
     private deliveryFee: number = 0;
     private address: any = {};
+    private order_Type: any = orderType;
     constructor(private storageService: StorageService, private globals: Globals) {
 
     }
 
     ngOnInit() {
         this.cartDetails = <CartItem>this.storageService.read('cartItems');
+        this.deliveryAddress = <Address>this.storageService.read('deliveryAddress');
         this.deliveryFee = 0;
     }
 
     populateDeliveryAddress = function () {
         if (this.address.SameAsDelivery) {
             //populate from delivery address
-            this.address = <Address>this.storageService.read('deliveryAddress');
+            this.address = this.deliveryAddress;
         } else {
             //clear address controls
         }

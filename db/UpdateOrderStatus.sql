e819b049a49d73c25be25fb6ae98166a939dc1ac
Gurpreet Singh <gopisivia@gmail.com>
2017-10-11 12:06:03 +0530

    Added sproc for updating status. Will be used for rejecting order

---
+++ db/UpdateOrderStatus.sql
@@ -0,0 +1,22 @@
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Gurpreet Singh
+-- Create date: 10/Oct/2017
+-- Description:	This sproc will Update Status for Order
+-- =============================================
+CREATE PROCEDURE UpdateOrderStatus
+	@OrderId int,
+	@Status int
+AS
+BEGIN
+	SET NOCOUNT ON;
+	UPDATE [dbo].[Order]
+	SET [Status] = @Status
+	WHERE [OrderId] = @OrderId
+END
+GO
+
